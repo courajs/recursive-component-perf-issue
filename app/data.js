15f3be56ea740eef6c8b598dce3cc1b803b400c3
Aaron Sikes <aaron@sikes.io>
2017-05-25 17:13:30 -0400

    Big data

--- app/data.js
+++ app/data.js
@@ -1,3 +1,25 @@
-export default {
-  key: 'value'
-};
+const base = 5;
+
+function gen(depth) {
+  if (depth <= 0) {
+    let result = {};
+    for (var i = 1; i <= base; i++) {
+      result["key"+i] = "value"+i;
+    }
+    return result;
+  } else if (depth % 2 === 1) {
+    let result = [];
+    for (var i = 1; i <= base; i++) {
+      result.push(gen(depth -1));
+    }
+    return result;
+  } else {
+    let result = {};
+    for (var i = 1; i <= base; i++) {
+      result["key"+i] = gen(depth-1);
+    }
+    return result;
+  }
+}
+
+export default gen(3);
